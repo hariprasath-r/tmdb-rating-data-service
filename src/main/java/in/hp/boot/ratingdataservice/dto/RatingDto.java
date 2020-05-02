af53a69ce477433b29865a09157ea6cdbe944795
Hariprasath <balaprasad24@gmail.com>
2020-05-02 15:49:51 +0530

    Added ApiModel details

--- src/main/java/in/hp/boot/ratingdataservice/dto/RatingDto.java
+++ src/main/java/in/hp/boot/ratingdataservice/dto/RatingDto.java
@@ -1,6 +1,8 @@
 package in.hp.boot.ratingdataservice.dto;
 
 import in.hp.boot.ratingdataservice.models.RatingModel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +10,11 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@ApiModel("User Ratings")
 public class RatingDto {
+
     private String userId;
+
+    @ApiModelProperty(position = 1)
     private List<RatingModel> ratings;
 }
--- src/main/java/in/hp/boot/ratingdataservice/models/RatingModel.java
+++ src/main/java/in/hp/boot/ratingdataservice/models/RatingModel.java
@@ -1,11 +1,18 @@
 package in.hp.boot.ratingdataservice.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@ApiModel("Rating")
 public class RatingModel {
+
+    @ApiModelProperty(position = 0)
     private String movieId;
+
+    @ApiModelProperty(position = 1)
     private Double rating;
 }
