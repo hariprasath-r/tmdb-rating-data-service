0544a737ddefd566f8d9aebc443af8cfa3d0c2b6
Hariprasath <balaprasad24@gmail.com>
2020-05-02 15:50:08 +0530

    Configured Watchlist Controller

--- src/main/java/in/hp/boot/ratingdataservice/controllers/WatchlistController.java
+++ src/main/java/in/hp/boot/ratingdataservice/controllers/WatchlistController.java
@@ -2,6 +2,9 @@ package in.hp.boot.ratingdataservice.controllers;
 
 import in.hp.boot.ratingdataservice.dto.WatchlistDto;
 import in.hp.boot.ratingdataservice.services.WatchlistService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,11 @@ public class WatchlistController {
     @Autowired
     private WatchlistService watchlistService;
 
+    @ApiOperation(value = "Adding a movie to user watchlist")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully added."),
+            @ApiResponse(code = 409, message = "User Already has movie in his list.")
+    })
     @PostMapping("/{userId}/{movieId}")
     public ResponseEntity<Object> addMovieToWatchlistForUser(@PathVariable String userId, @PathVariable String movieId) {
         watchlistService.addMovieToUserWatchlist(userId, movieId);
@@ -25,17 +33,32 @@ public class WatchlistController {
         return ResponseEntity.created(uri).build();
     }
 
+    @ApiOperation(value = "Getting all movies for a user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved.", response = WatchlistDto.class),
+            @ApiResponse(code = 404, message = "User not found.")
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<WatchlistDto> getWatchlistForUser(@PathVariable String userId) {
         return ResponseEntity.ok(watchlistService.getWatchlistForUser(userId));
     }
 
+    @ApiOperation(value = "Deleting all movies for a user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Will be deleted shortly."),
+            @ApiResponse(code = 404, message = "User not found.")
+    })
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteAllMoviesForUser(@PathVariable String userId) {
         watchlistService.deleteAllMoviesForUser(userId);
         return ResponseEntity.accepted().build();
     }
 
+    @ApiOperation(value = "Deleting all movies for a user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Will be deleted shortly."),
+            @ApiResponse(code = 404, message = "User or movie is not found.")
+    })
     @DeleteMapping("/{userId}/{movieId}")
     public ResponseEntity<Object> deleteMovieFromUser(@PathVariable String userId, @PathVariable String movieId) {
         watchlistService.deleteMovieFromUser(userId, movieId);
