73079fe7c3cd472b8b7817d9c60a95d50dfbe2fa
Hariprasath <balaprasad24@gmail.com>
2020-05-02 11:56:32 +0530

    Update uri for watchlist

--- src/main/java/in/hp/boot/ratingdataservice/controllers/WatchlistController.java
+++ src/main/java/in/hp/boot/ratingdataservice/controllers/WatchlistController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/wishlist")
+@RequestMapping("/v1/watchlist")
 public class WatchlistController {
 
     @Autowired
